d367ac0fa5aa3d747a808148d740a5ea537274aa
Hunter Rancourt <skwid138@gmail.com>
2017-12-10 13:51:34 -0600

    setup http call on service

---
+++ src/app/entries/shared/entry.model.ts
@@ -0,0 +1,8 @@
+// export component
+export class Entry {
+    // variables to be displayed on the DOM
+    title: string;
+    photo: string;
+    description: string;
+    comments: any[];
+} // end export
--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -1,15 +1,36 @@
 // minimum required for an angular service is
 // a class and the @injectable decorator
 
+//import entry model
+import { Entry } from './entry.model';
+
 // import injectable decorator from angular core
 import { Injectable } from "@angular/core";
+
+// import http service
+import { Http } from '@angular/http';
 
 // injectable decorator
 @Injectable()
 
 // export service
 export class EntryService {
-    getEntries() {
 
+    // using the keyword private or public allows the http
+    // service to be accessed outside of the constructor
+    constructor(private http: Http) {
+
+    } // end constructor
+
+    // defining return to be a promise with an array of Entrys
+    // this is known as a generic type which is a type within a type
+    // a promise type containing an array of entry types
+    getEntries(): Promise<Entry[]> {
+        // url for entries api
+        return this.http.get('/app/entries')
+            .toPromise()
+            // if you don't wrap in curly brackets the expression
+            // is returned the as operator defines the type of the data object
+            .then(response => response.json().data as Entry[]);
     } // end getEntries
 } // end export
