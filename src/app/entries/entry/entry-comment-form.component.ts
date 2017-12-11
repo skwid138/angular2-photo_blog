11629a2cfbeb091497abdacd5634deb4f1cfddcf
Hunter Rancourt <skwid138@gmail.com>
2017-12-10 18:56:15 -0600

    setup view child decorator and have form clearing inputs after data submit is clicked

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,6 +1,13 @@
-// import component, event emitter, and output from angular core
-import { Component, EventEmitter, Output } from '@angular/core';
+// import component, event emitter, output, and view child from angular core
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 // event emitter is a way to pass data from one component to the next
+// output lets us capture data
+// the viewchild decorator lets the component inspect the template
+// for local variables - #[nameHere]="something" in the html
+// and assigns the local variables as member properties of the component
+
+// import ngForm
+import { NgForm } from '@angular/forms';
 
 // component decorator
 @Component({
@@ -15,10 +22,16 @@ export class EntryCommentFormComponent {
 
     // event emitter uses generics to define types <{}>
     @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
+
+    // bound to local variable on template and type format
+    @ViewChild('commentForm') commentForm: NgForm; 
+
     // binding for form submission
-    onSubmit() {
+    onSubmit(commentForm: NgForm) {
         let comment = { name: this.name, comment: this.comment};
         // emit comment when submit is clicked
         this.onCommentAdded.emit(comment);
+        // clear form inputs when form is submitted
+        this.commentForm.resetForm();
     } // end onSubmit
 } // end export
